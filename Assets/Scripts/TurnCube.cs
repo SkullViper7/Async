dcf9a969be8b233c7d6a6a92f0aa94a009f5174c
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-01-25 12:03:59 +0100

    comments

--- Assets/Scripts/TurnCube.cs
+++ Assets/Scripts/TurnCube.cs
@@ -11,11 +11,18 @@ public class TurnCube : MonoBehaviour
 
     Coroutine _coroutine;
 
+    /// <summary>
+    /// Starting the coroutine and storing it in a variable.
+    /// </summary>
     public void StartTurn()
     {
         _coroutine = StartCoroutine(Turn());
     }
 
+    /// <summary>
+    /// Coroutine to rotate the cube 1° every 0.01s, you can choose for how many seconds you want the cube to rotate.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Turn()
     {
         for (int i = 0; i < _rotationTime * 100; i++)
@@ -25,6 +32,9 @@ public class TurnCube : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Stops the coroutine.
+    /// </summary>
     public void Stop()
     {
         StopCoroutine(_coroutine);
--- Assets/Scripts/TurnCubeAsync.cs
+++ Assets/Scripts/TurnCubeAsync.cs
@@ -11,11 +11,18 @@ public class TurnCubeAsync : MonoBehaviour
 
     bool _isCanceled;
 
+    /// <summary>
+    /// Starts the async task.
+    /// </summary>
     public void StartTask()
     {
         RotateCube();
     }
 
+    /// <summary>
+    /// Same thing as the coroutine unless this time, if a bool is true, the task is stoped.
+    /// </summary>
+    /// <returns></returns>
     async Task RotateCube()
     {
         for (int i = 0; i < _rotationTime * 100; i++)
@@ -30,6 +37,9 @@ public class TurnCubeAsync : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Setting a bool to stop the task.
+    /// </summary>
     public void StopTask()
     {
         _isCanceled = true;
